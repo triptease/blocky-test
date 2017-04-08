1e81a8a701b1b0de5bf6d7ff27d6fcb6c16f90b3
Johnny Magrippis <j@magrippis.com>
2017-04-08 10:48:22 +0100

    Moved served files in `public` directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public/dist')
   },
   module: {
     rules: [
@@ -26,6 +26,7 @@ module.exports = {
     ]
   },
   devServer: {
+    contentBase: path.join(__dirname, 'public'),
     publicPath: '/dist/',
     port: 9100
   }

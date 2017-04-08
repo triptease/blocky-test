c1e5dba9d86699ebf4835f123a7a6b2558c7b3c7
Johnny Magrippis <j@magrippis.com>
2017-04-08 08:09:18 +0100

    Runs code through prettier

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -3,56 +3,56 @@ const MAX_X = 10;
 const MAX_Y = 10;
 
 export class Block {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
-        this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+  }
 }
 
 export class BlockGrid {
-    constructor () {
-        this.grid = [];
+  constructor() {
+    this.grid = [];
 
-        for (let x = 0; x < MAX_X; x++) {
-            let col = [];
-            for (let y = 0; y < MAX_Y; y++) {
-                col.push(new Block(x, y));
-            }
+    for (let x = 0; x < MAX_X; x++) {
+      let col = [];
+      for (let y = 0; y < MAX_Y; y++) {
+        col.push(new Block(x, y));
+      }
 
-            this.grid.push(col);
-        }
-
-        return this;
+      this.grid.push(col);
     }
 
-    render (el = document.querySelector('#gridEl')) {
-        for (let x = 0; x < MAX_X; x++) {
-            let id = 'col_' + x;
-            let colEl = document.createElement('div');
-            colEl.className = 'col';
-            colEl.id = id;
-            el.appendChild(colEl);
+    return this;
+  }
 
-            for (let y = MAX_Y - 1; y >= 0; y--) {
-                let block = this.grid[x][y],
-                    id = `block_${x}x${y}`,
-                    blockEl = document.createElement('div');
+  render(el = document.querySelector('#gridEl')) {
+    for (let x = 0; x < MAX_X; x++) {
+      let id = 'col_' + x;
+      let colEl = document.createElement('div');
+      colEl.className = 'col';
+      colEl.id = id;
+      el.appendChild(colEl);
 
-                blockEl.id = id;
-                blockEl.className = 'block';
-                blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));
-                colEl.appendChild(blockEl);
-            }
-        }
+      for (let y = MAX_Y - 1; y >= 0; y--) {
+        let block = this.grid[x][y],
+          id = `block_${x}x${y}`,
+          blockEl = document.createElement('div');
 
-        return this;
+        blockEl.id = id;
+        blockEl.className = 'block';
+        blockEl.style.background = block.colour;
+        blockEl.addEventListener('click', evt => this.blockClicked(evt, block));
+        colEl.appendChild(blockEl);
+      }
     }
 
-    blockClicked (e, block) {
-        console.log(e, block);
-    }
+    return this;
+  }
+
+  blockClicked(e, block) {
+    console.log(e, block);
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());

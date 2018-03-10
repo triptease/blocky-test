20da05f42fa1425aaf3c352c4dd79363a069ecd9
Johnny Magrippis <j@magrippis.com>
2018-03-10 10:44:06 +0000

    Latest webpack, babel and jest

--- src/Block.test.js
+++ src/Block.test.js
@@ -1,15 +1,14 @@
 import Block, { COLOURS } from './Block';
-import { assert } from 'chai';
 
 describe('Block', () => {
   it('should be created with correct coordinates and one of the valid colours', () => {
-    let testCoords = [[1, 2], [4, 9], [0, 0]];
+    const testCoords = [[1, 2], [4, 9], [0, 0]];
 
     testCoords.forEach(testCoord => {
-      let block = new Block(...testCoord);
-      assert.equal(block.x, testCoord[0], 'x is set correctly');
-      assert.equal(block.y, testCoord[1], 'y is set correctly');
-      assert.ok(COLOURS.indexOf(block.colour) > -1, 'colour is valid');
+      const block = new Block(...testCoord);
+      expect(block.x).toBe(testCoord[0]);
+      expect(block.y).toBe(testCoord[1]);
+      expect(COLOURS).toContain(block.colour);
     });
   });
 });
